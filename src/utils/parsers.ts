fd8c0d96db693b08866075d406b3a12524c063fa
Jonatan966 <jonatanfrederico@gmail.com>
2021-02-05 23:57:17 -0300

    🛠 Adjustment: Ajustes no delimitador de campos Agora os campos que não existem serão removidos por completo

--- src/utils/parsers.ts
+++ src/utils/parsers.ts
@@ -1,14 +1,20 @@
 export function parseQueryParams(query, acceptOnly: string[]) {
   let newQuery = {};
 
+  Object.keys(query).forEach(item => {
+    if (!(acceptOnly.findIndex(obj => obj === item) + 1)) {
+      delete query[item];
+    }
+  });
+
   if (Object.keys(query).length) {
     acceptOnly.forEach(item => {
       if (Number(query[item]) === 1) {
         return;
       }
-      newQuery[item] = 0;
+      newQuery[item] = 0;  
     });  
   }
-  
+
   return newQuery;
 }

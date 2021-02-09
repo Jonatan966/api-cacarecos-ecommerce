193461a985ee5ca4c39187c4442b8f495688685b
Jonatan966 <jonatanfrederico@gmail.com>
2021-02-09 19:43:10 -0300

    💡Feature: Função para filtrar campos Essa função monta um JSON que será usado para filtrar pesquisas

--- src/utils/parsers.ts
+++ src/utils/parsers.ts
@@ -33,3 +33,20 @@ export function parsePaginator(page: any, limit: any, max_results: number = 15) 
 
   return finalResult;
 }
+
+export function parseSearchFilter(query: any, acceptOnly: string[], notUseRegexQuery?: string[]) {
+  const {find_by, find_query} = query;
+  let finalFilter = {};
+
+  if (find_by && find_query) {
+    const item = acceptOnly.find(item => item === find_by);
+    if (item) {
+      finalFilter[item] = 
+        !!notUseRegexQuery.find(queryItem => queryItem === item) 
+        ? find_query 
+        : {$regex: `.*${find_query}.*`};
+    }  
+  }
+
+  return finalFilter;
+}

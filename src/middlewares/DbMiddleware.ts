9d0ad2b8b38db121b6ade8a36de4bc6db47a04cb
Jonatan966 <jonatanfrederico@gmail.com>
2021-02-17 16:22:25 -0300

    🛠 Adjustment: Token autorizado para envio

--- src/middlewares/DbMiddleware.ts
+++ src/middlewares/DbMiddleware.ts
@@ -4,6 +4,7 @@ import connectToDatabase from "../connectors/DatabaseConnector";
 export default function DbMiddleware(func: (req: NowRequest, res: NowResponse) => any) {
     return async (req: NowRequest, res: NowResponse) => {
         await connectToDatabase();
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         
         if (req.method === 'OPTIONS') {
             return res.status(200).end();

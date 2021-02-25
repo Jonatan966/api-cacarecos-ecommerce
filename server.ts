dc538f03c1cee5f340ce16d067effa438173b360
Jonatan966 <jonatanfrederico@gmail.com>
2021-02-25 20:35:32 -0300

    💡Feature: Criação do arquivo de inicialização Arquivo de inicialização do servidor express

---
+++ server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import defineRoutes from './src/utils/defineRoutes';
+
+dotenv.config();
+
+const server = express();
+
+defineRoutes(server);
+
+server.get('/*', async (req,res) => {
+  return res.status(404).json({error: 'ROTA INEXISTENTE'});
+});
+
+server.listen(process.env.PORT || 5001, () => console.log('Servidor aberto!'));

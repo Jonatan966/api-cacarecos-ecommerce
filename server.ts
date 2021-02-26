3b95083f26b391dc4619c2f9320824e7c85e8bf9
Jonatan966 <jonatanfrederico@gmail.com>
2021-02-25 21:29:25 -0300

    testes

--- server.ts
+++ server.ts
@@ -13,4 +13,8 @@ server.get('/*', async (req,res) => {
   return res.status(404).json({error: 'ROTA INEXISTENTE'});
 });
 
+server.all('/teste', (req, res) => {
+  return res.send('TESTE');
+});
+
 server.listen(process.env.PORT || 5001, () => console.log('Servidor aberto!'));

6a1f71280391a1297ceb73aac6376643e4f5c7d6
Jonatan966 <jonatanfrederico@gmail.com>
2021-02-27 14:49:49 -0300

    feature: middleware do banco de dados na rota all Agora o banco de dados estará conectado em todas as rotas

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import defineRoutes from './src/utils/defineRoutes';
+import DbMiddleware from './src/middlewares/DbMiddleware';
 
 //import dotenv from 'dotenv';
 //dotenv.config();
@@ -9,6 +10,7 @@ server.use(express.json());
 
 const serverPort = process.env.PORT || 5000;
 
+server.all('/api/*', DbMiddleware);
 defineRoutes(server);
 
 server.listen(serverPort, () => 

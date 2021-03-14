3af1ded29b67445beecbc35daac19cbaa38dec0f
Jonatan966 <jonatanfrederico@gmail.com>
2021-03-14 15:52:33 -0300

    feature: separação de responsabilidades

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,7 @@
-import express from 'express'
-import cors from 'cors'
-
-import { routes } from './routes'
-
-const server = express()
-
-server.use(express.json())
-server.use(express.urlencoded({ extended: true }))
-server.use(cors())
+import { app } from './app'
 
 const serverPort = process.env.PORT || 5000
 
-server.use(routes)
-
-server.listen(serverPort, () =>
+app.listen(serverPort, () =>
   console.log(`Servidor aberto na porta ${serverPort}`)
 )

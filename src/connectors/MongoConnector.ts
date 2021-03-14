dbac65147dd4862e6303b2c339aaefc5780c0b45
Jonatan966 <jonatanfrederico@gmail.com>
2021-03-14 18:43:51 -0300

    feature: mudanças nas variáveis de ambiente

--- src/connectors/MongoConnector.ts
+++ src/connectors/MongoConnector.ts
@@ -7,12 +7,12 @@ export class MongoConnector {
     if (db) {
       return db
     }
-    const client = await MongoClient.connect(process.env.DB, {
+    const client = await MongoClient.connect(process.env.MONGODB_CLUSTER_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
 
-    db = client.db('cacarecos')
+    db = client.db(process.env.NODE_ENV === 'test' ? process.env.MONGODB_TEST_DB : process.env.MONGODB_MAIN_DB)
 
     return db
   }
